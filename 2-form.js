c1224c84f60dc9eea4304948a55241aa94a3af8e
rssua <rssuand@gmail.com>
2025-01-28 20:23:46 +0000

    Deploying to gh-pages from @ rssua/goit-js-hw-09@9f611b72a4ff6196e81c292aacca360e7f76b555 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */let e="1er",o=["www",577,!0,e];console.log(o);let s=JSON.stringify(o);console.log(s);console.log(...o);let t={...o};console.log(t);let l={ff:"fdg5",sds:77};console.dir(l);console.log(l);
+//# sourceMappingURL=2-form.js.map
