3d061950ea44ce270b826ce739d6fcea0de2f661
Andrii Rudenko <k615wadem@mozmail.com>
2025-01-30 01:55:37 +0200

    form finished

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,28 +1,50 @@
-let dd = '1er';
-let gg = ['www', 577, true, dd];
-console.log(gg);
-let jst = JSON.stringify(gg);
+const refs = { form: document.querySelector('.feedback-form') };
 
-console.log(jst);
+const LSKEY = 'feedback-form-state';
 
-console.log(...gg);
-let cc = { ...gg };
-console.log(cc);
-let yy = { ff: 'fdg5', sds: 77 };
-console.dir(yy);
-console.log(yy);
+let formData = { email: '', message: '' };
 
-function saveToLS(key, value) {
+initForm();
+
+refs.form.addEventListener('input', e => {
+  formData.email = e.currentTarget.elements.email.value.trim();
+  formData.message = e.currentTarget.elements.message.value.trim();
+  putInLS(LSKEY, formData);
+});
+
+//* Cleaning on Submit
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem(LSKEY);
+  formData.email = formData.message = '';
+  refs.form.reset();
+});
+
+//* Restore Form on Reload
+function initForm() {
+  formData = takeFromLS(LSKEY) || formData;
+  refs.form.elements.email.value = formData.email;
+  refs.form.elements.message.value = formData.message;
+}
+
+//* Data to Local Storage
+function putInLS(key, value) {
   const jsonData = JSON.stringify(value);
   localStorage.setItem(key, jsonData);
 }
 
-function loadFromLS(key) {
-  const body = localStorage.getItem(key);
+//* Data from Local Storage
+function takeFromLS(key) {
+  const value = localStorage.getItem(key);
   try {
-    const data = JSON.parse(body);
+    const data = JSON.parse(value);
     return data;
   } catch {
-    return body;
+    return value;
   }
 }
